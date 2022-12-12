525ea1df1f6246d1b1ff70dab93e06d7734efe47
Vladislav Iampolskii <cakefronend@gmail.com>
2022-12-12 22:43:17 +0400

    card create module

--- index.js
+++ index.js
@@ -1,24 +1,71 @@
-let buttons = document.querySelectorAll(".open-popup");
-let popup = document.querySelector(".popup");
-let buttonClose = popup.querySelector(".close");
-let popupBlock = popup.querySelector(".block");
+const buttons = document.querySelectorAll(".open-popup")
+const popup = document.querySelector(".popup")
+const buttonClose = document.querySelector(".close")
+const popupBlock = document.querySelector(".block")
 
 function openPopup() {
-  popup.classList.add("open-popup");
+  popup.classList.add("open-popup")
 }
 
 function closePopup() {
-  popup.classList.remove("open-popup");
+  popup.classList.remove("open-popup")
 }
 
 buttons.forEach((button) => {
-  button.addEventListener("click", openPopup);
-});
+  button.addEventListener("click", openPopup)
+})
 
-buttonClose.addEventListener("click", closePopup);
+buttonClose.addEventListener("click", closePopup)
 
-popup.addEventListener("click", closePopup);
+popup.addEventListener("click", closePopup)
 
 popupBlock.addEventListener("click", (event) => {
-  event.stopPropagation();
-});
+  event.stopPropagation()
+})
+
+const card = (function(){
+  const input = document.querySelector('input[type="text"].note')
+  const form = document.querySelector('.note-form')
+  const box = document.querySelector('.notes-container')
+
+  const init = () => {
+    form.addEventListener('submit', submit)
+    box.addEventListener('click', removeCard)
+  }
+
+  const createCard = (text) => {
+    const card = document.createElement('div')
+    const button = document.createElement('button')
+    const textNode = document.createElement('div')
+
+    card.classList.add('border-card')
+    button.classList.add('button-cross')
+    textNode.classList.add('text-12')
+
+    textNode.innerHTML = text
+
+    card.appendChild(button)
+    card.appendChild(textNode)
+
+    box.prepend(card)
+  }
+
+  const removeCard = (e) => {
+    const isDeleteButton = e.target.classList.contains('button-cross') 
+    
+    if(isDeleteButton) {
+      e.target.parentNode.remove()
+    }
+  }
+
+  const submit = (e) => {
+    e.preventDefault()
+    
+    createCard(input.value)
+  }
+
+  return {
+    init,
+    createCard
+  }
+})()
