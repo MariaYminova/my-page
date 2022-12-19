26d853c98e90da4593f3193f42aa3740620d25d0
MariaYminova <vabylia96@gmail.com>
2022-12-19 15:45:57 +0400

    technical edits

---
+++ css/Untitled-1.js
@@ -0,0 +1,12 @@
+// Write your code below
+const bobsFollowers = ['masha', 'glasha', 'rastopasha', 'dyrasha'];
+const tinasFollowers = ['masha', 'glasha', 'trasha'];
+const mutualFollowers = [];
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+
+    }
+  }
+}
+
--- index.js
+++ index.js
@@ -22,3 +22,56 @@ popup.addEventListener("click", closePopup);
 popupBlock.addEventListener("click", (event) => {
   event.stopPropagation();
 });
+
+
+
+
+
+
+const card = (function () {
+  const input = document.querySelector('input[type="text"].note')
+  const form = document.querySelector('.note-form')
+  const box = document.querySelector('.notes-container')
+
+  const init = () => {
+    form.addEventListener('submit', submit)
+    box.addEventListener('click', removeCard)
+  }
+
+  const createCard = (text) => {
+    const card = document.createElement('div')
+    const button = document.createElement('button')
+    const textNode = document.createElement('div')
+
+    card.classList.add('border-card')
+    button.classList.add('button-cross')
+    textNode.classList.add('text-12')
+
+    textNode.innerHTML = text
+
+    card.appendChild(button)
+    card.appendChild(textNode)
+
+    return card
+  }
+
+  const removeCard = (e) => {
+    const isDeleteButton = e.target.classList.contains('button-cross')
+
+    if (isDeleteButton) {
+      e.target.parentNode.remove()
+    }
+  }
+
+  const displayCard = (card) => {
+    box.prepend(card)
+  }
+  const submit = (e) => {
+    e.prependDefault()
+
+    displayCard(createCard(input.value))
+  }
+  return {
+    init
+  }
+})()
